8cba77e536356e1336bf569001400798c4e7e0b5
purbaronny <95927137+purbaronny@users.noreply.github.com>
2025-02-05 09:45:16 +0700

    feat(calculatNumberSeries) finish development

--- src/main/java/com/purwadhika/Practice03.java
+++ src/main/java/com/purwadhika/Practice03.java
@@ -3,7 +3,18 @@ package com.purwadhika;
 public class Practice03 {
 
     public long calculateNumberSeries(int value) {
+        if(value < 0) {
+            throw new IllegalArgumentException("value must be positive integer");
+        }
 
-        return 0;
+        String text = String.valueOf(value);
+        char[] chs = text.toCharArray();
+
+        long result = 0;
+        for(char ch : chs) {
+            int temp = Integer.valueOf(String.valueOf(ch));
+            result += temp;
+        }
+        return result;
     }
 }
